c3b9b95abc1ca741908258795e081e816b1fa51d
stephenhu990622 <72297600+stephenhu990622@users.noreply.github.com>
2021-02-04 18:45:04 +0000

    Add files via upload

--- Robopump2/app/src/main/java/com/example/robopump2/MainActivity.java
+++ Robopump2/app/src/main/java/com/example/robopump2/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.robopump2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_page);
+        setContentView(R.layout.activity_main);
+        TextView textView= (TextView)findViewById(R.id.current_amount);
+        SeekBar seekBar= (SeekBar)findViewById(R.id.fuel_amount_slider);
+
+
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText(String.valueOf(progressChangedValue)+"L") ;
+            }
+        });
+
     }
+
 }
