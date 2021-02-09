0bb2ad26fca6af533577313a1be46e597a1a616a
Ziemniok352 <wot.jasio@gmail.com>
2021-02-09 15:21:54 +0000

    Added parsing of user input

--- Robopump2/app/src/main/java/com/example/robopump2/SettingsActivity.java
+++ Robopump2/app/src/main/java/com/example/robopump2/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.robopump2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -12,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SettingsActivity extends AppCompatActivity {
 
     private ImageButton returnButton;
+    private EditText nameInput, emailInput, postcodeInput, cardInput, CVCInput, expiryInput;
+    private String name, email, postcode, cardNumber, expiryDate;
+    private int CVC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,33 @@ public class SettingsActivity extends AppCompatActivity {
     public void returnToMainPage() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    // Method to create a user record from the users input
+    // This will probably be called when add user button is clicked
+    public UserInformation addUser() {
+
+        // Read text input
+        nameInput = (EditText) findViewById(R.id.name);
+        emailInput = (EditText) findViewById(R.id.email);
+        postcodeInput = (EditText) findViewById(R.id.postcode);
+        cardInput = (EditText) findViewById(R.id.card_number);
+        CVCInput = (EditText) findViewById(R.id.CVC);
+        expiryInput = (EditText) findViewById(R.id.expiry_date);
+
+        // Parse user input into variables
+        name = nameInput.getText().toString();
+        email = emailInput.getText().toString();
+        postcode = postcodeInput.getText().toString();
+        cardNumber = cardInput.getText().toString();
+        CVC = Integer.valueOf(CVCInput.getText().toString());
+        expiryDate = expiryInput.getText().toString();
+
+        // Create a new use from the inputted information
+        UserInformation user = new UserInformation(name,email,postcode,
+                new CardInformation(cardNumber, CVC, expiryDate));
+
+        return user;
     }
 }
 
