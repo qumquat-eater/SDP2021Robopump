7176aafd863b8313c06bf2e2b8b9075bf793d3d6
Xinchen Hu <xinchenhu@Xinchens-MacBook-Pro.local>
2021-02-09 16:20:17 +0000

    finish order summary output on setting page

--- Robopump2/app/src/main/java/com/example/robopump2/SettingsActivity.java
+++ Robopump2/app/src/main/java/com/example/robopump2/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.example.robopump2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -12,6 +15,15 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SettingsActivity extends AppCompatActivity {
 
     private ImageButton returnButton;
+    private Button commitButton;
+    private EditText name;
+    private EditText email;
+    private EditText postcode;
+    private EditText card_number;
+    private TextView summary;
+    public static final String SHARED_PREF = "shared";
+    public static final String TEXT = "text";
+    public String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,45 @@ public class SettingsActivity extends AppCompatActivity {
                 returnToMainPage();
             }
         });
+
+        //commit changes and output changes into Account Summary
+        commitButton = (Button)findViewById(R.id.commit_changes);
+        name = (EditText)findViewById(R.id.name);
+        email = (EditText)findViewById(R.id.email);
+        postcode = (EditText)findViewById(R.id.postcode);
+        card_number = (EditText)findViewById(R.id.card_number);
+        summary = (TextView)findViewById(R.id.account_summary);
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name1 = name.getText().toString();
+                String email1 = email.getText().toString();
+                String postcode1 = postcode.getText().toString();
+                String card_number1 = card_number.getText().toString();
+                summary.setText("Account Summary:"+
+                                "\n\nName: "+name1+
+                                "\nEmail: "+email1+
+                                "\nPostcode: "+postcode1+
+                                "\nCard Number: "+card_number1);
+
+                //save values in viewText and prevent values disappearing once users click back to main page
+                SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF,MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(TEXT,summary.getText().toString());
+                editor.apply();
+
+            }
+        });
+
+        update();
+
+
+    }
+    //helper function
+    private void update() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT,"");
+        summary.setText(text);
     }
 
     public void returnToMainPage() {
